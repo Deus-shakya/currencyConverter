fa5adc44d043793fecc2ff1c97e2ebd8bef8f7ce
Deus-shakya <shakyasweekar0@gmail.com>
2024-03-23 19:56:01 +0545

    using GPT template with own edit

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-let options = document.querySelectorAll(".dropdown select")
+const api = "https://v6.exchangerate-api.com/v6/0b2c94015ff32fce2aedc4c7/pair/USD/NPR"
+
+
+
+
+let options = document.querySelectorAll(".input-group select")
 
 for(option of options){
   
